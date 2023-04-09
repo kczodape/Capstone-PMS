7514ede0c24886a9fca105ef4cb5e5d30b5ba960
kczodape <krunalzodape9@gmail.com>
2023-04-09 11:15:41 +0530

    microservice deleted

--- Microservices/g3-patient-info-service/src/main/java/com/capstone/application/repository/PatientInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.application.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capstone.application.model.Patient;
-
-public interface PatientInfoRepository extends JpaRepository<Patient,Integer>{
-
-}
